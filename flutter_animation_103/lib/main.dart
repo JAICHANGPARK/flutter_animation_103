2671685f20fa0178c99af773b9efeabc91efbfc0
Dreamwalker <aristojeff@gmail.com>
2018-12-11 11:33:52 +0900

    rotateY Code

--- flutter_animation_103/lib/main.dart
+++ flutter_animation_103/lib/main.dart
@@ -63,17 +63,20 @@ class _MyHomePageState extends State<MyHomePage>
                   child: Transform(
                     transform: Matrix4.identity()
                       ..setEntry(3, 2, 0.005)
-                      ..rotateX(2 * pi * flip_anim.value),
+                      ..rotateY(2 * pi * flip_anim.value),
                     alignment: Alignment.center,
                     child: Container(
                       height: 100.0,
                       width: 100.0,
                       color: Colors.green.withOpacity(0.2),
-                      child: Center(
-                        child: Text(
-                          'Flip',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 25.0),
+                      child: RotationTransition(
+                        turns: flip_anim,
+                        child: Center(
+                          child: Text(
+                            'Flip',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 25.0),
+                          ),
                         ),
                       ),
                     ),
